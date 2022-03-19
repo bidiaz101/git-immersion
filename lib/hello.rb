1556ea4abf6799ff6b736a7621bd2092f520e1df
Brian Diaz <bidiaz101@gmail.com>
2022-03-19 15:19:05 -0500

    Made interactive

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,4 +1,4 @@
-# Default is "World"
-# Author: Brian Diaz (bidiaz101@gmail.com)
-name = ARGV.first || "World"
-puts "Hello, #{name}!"
+puts "What's your name?"
+my_name = gets.strip
+
+puts "Hello, #{my_name}!"
