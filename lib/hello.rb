ab9a515c648521e9648253111140d80a6afe8135
Brian Diaz <bidiaz101@gmail.com>
2022-03-19 15:08:09 -0500

    Hello uses Greeter

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,4 +1,7 @@
 # Default is "World"
 # Author: Brian Diaz (bidiaz101@gmail.com)
+require 'greeter'
+
 name = ARGV.first || "World"
-puts "Hello, #{name}!"
+greeter = Greeter.new(name)
+puts greeter.greet
